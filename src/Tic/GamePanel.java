46a574bcbf46d338840af4be87f00355cec11933
student <student@ip-172-31-50-238.us-west-2.compute.internal>
2021-07-28 18:26:32 -0700

    jjkjkj

--- src/Tic/GamePanel.java
+++ src/Tic/GamePanel.java
@@ -3,15 +3,18 @@ package Tic;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 
-public class GamePanel extends JPanel implements KeyListener, MouseListener {
+public class GamePanel extends JPanel implements KeyListener, MouseListener, ActionListener {
 	final int MENU = 0;
 	final int P1 = 1;
 	final int P2 = 2;
@@ -20,11 +23,15 @@ public class GamePanel extends JPanel implements KeyListener, MouseListener {
 	Font titlefont = new Font("Arial", Font.PLAIN, 48);
 	Font smolFont = new Font("Arial", Font.PLAIN, 24);
 	char[][] grid;
+	Timer framedraw = new Timer(1000/60, this);
 	public GamePanel() {
 		grid = new char[3][3];
-		
-		
-		
+		framedraw.start();
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				grid[i][j]= ' ' ;
+			}
+		}
 	}
 	@Override
 	public void paintComponent(Graphics g) {
@@ -32,25 +39,27 @@ public class GamePanel extends JPanel implements KeyListener, MouseListener {
 		g.setColor(Color.white);
 		g.fillRect(0, 0, Tic.WIDTH, Tic.LENGTH);
 		g.setColor(Color.black);
-		if (currentState == MENU) {
+		if (currentState == 0) {
 			drawMenuState(g);
 		}
-		if (currentState == P1) {
-			for (int i = 0; i < grid.length; i++) {
+		if (currentState == 1 || currentState == 2) {
+			for (int i =0; i < grid.length; i++) {
 				for (int j = 0; j < grid[i].length; j++) {
-					grid[i][j]= ' ' ;
+					g.drawRect(j*Tic.WIDTH/3, i*Tic.LENGTH/3, Tic.WIDTH/3, Tic.LENGTH/3);
+					g.drawString(""+grid[j][i], j*Tic.WIDTH/3+75, i*Tic.LENGTH/3+150);
 					
 				}
 			}
-		}
-		for (int i =0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				g.drawRect(j*Tic.WIDTH/3, i*Tic.LENGTH/3, Tic.WIDTH/3, Tic.LENGTH/3);
-				g.drawString(""+grid[j][i], j*Tic.WIDTH/3+75, i*Tic.LENGTH/3+150);
-				
+			
+					
+				}
+		
+		if (currentState == 3){
+		drawEndState(g);
 			}
-		}
+		
 	}
+	
 	public void drawMenuState(Graphics g) {
 		g.setColor(Color.black);
 		g.setFont(titlefont);
@@ -58,9 +67,43 @@ public class GamePanel extends JPanel implements KeyListener, MouseListener {
 		g.setFont(smolFont);
 		g.drawString("Press Enter to start", 125, 300);
 	}
+	public void drawEndState(Graphics g) {
+		g.setColor(Color.black);
+		g.setFont(titlefont);
+		g.drawString("won!", 100, 100);
+		g.drawString("Press Enter to play again", 125, 300);
+	}
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
+		if (arg0.getKeyCode()==KeyEvent.VK_ENTER) {
+		    if (currentState == 3) {
+		        currentState = 0;
+	
+		    } 
+		    else if (currentState == 1 || currentState == 2) {
+		    	currentState = 3;
+		    }
+		    else {
+		        currentState++;
+		    }
+		}  
+		
+	}
+	public void checkMatch(int r, int c, int p) {
+		for (int i = 0; i<grid.length; i++) {
+			for (int j=0; j<grid[i].length; j++){
+				
+			}
+		}
+	}
+public void updateMenuState() {
+		
+	}
+	public void updateGameState() {
+		
+	}
+	public void updateEndState() {
 		
 	}
 
@@ -135,6 +178,11 @@ if (grid[x/(Tic.WIDTH/3)] [y/(Tic.LENGTH/3)]=='x' || grid[x/(Tic.WIDTH/3)][y/(Ti
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
 	}
 	
 	
